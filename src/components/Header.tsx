520d8510ae5f0f43e9fa7c445271c65cae94c807
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2026-05-09 15:11:06 +0530

    Add empty line for improved readability in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import { motion, AnimatePresence } from 'motion/react'
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
